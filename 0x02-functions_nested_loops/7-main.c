6a78541c36201cc0a017592e1cca6452bb8d3ce2
Samuel Abayomi <samuelabayomi230@gmail.com>
2023-06-21 13:34:37 -0700

    8-24

--- 0x02-functions_nested_loops/7-main.c
+++ 0x02-functions_nested_loops/7-main.c
@@ -3,7 +3,7 @@
 /**
  * main - check the code
  *
- * Return: Always 0.
+ * Return: Always 0
  */
 int main(void)
 {
@@ -14,5 +14,6 @@ int main(void)
     r = print_last_digit(-1024);
     _putchar('0' + r);
     _putchar('\n');
+
     return (0);
 }
