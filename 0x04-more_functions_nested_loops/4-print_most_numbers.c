fca2ffdacf1b94c6dff8df80b4f8ae7d5dc51c74
Samuel Abayomi <samuelabayomi230@gmail.com>
2023-06-23 06:00:39 -0700

    4-print_most_numbers.c

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -10,18 +10,12 @@ void print_most_numbers(void)
 
 {
 
-	int x = 0;
+	char x;
 
-	for (; x <= 0; x++)
+	for (x = '0'; x <= '9'; x++)
 	{
-	if (x == 2 || x == 4)
-	{
-	continue;
-	}
-	else
-	{
-	_putchar(x + '0');
-	}
+         	if (!(x == '2' || x == '4'))
+	                _putchar(x);
 	}
 	_putchar('\n');
 }
