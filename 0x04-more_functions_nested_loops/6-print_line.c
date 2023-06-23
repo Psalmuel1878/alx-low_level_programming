974cb2623c33b357d412333dac3d9d1c530d700e
Samuel Abayomi <samuelabayomi230@gmail.com>
2023-06-23 06:45:19 -0700

    6-print_line.c

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -3,25 +3,24 @@
 /**
  * print_line - Draws a straight line according to parameter
  * @n: The number of lines to draw
- * Return: empty
  */
 
 void print_line(int n)
 
-{
-	int x;
-	
+{	
 	if (n <= 0)
 	{
 	_putchar('\n');
 	}
 	else
+	{	
+	int x;
+
+	for (x = 1; x <= n; x++)
 	{
-	for (x = 0; x < n; x++)
-	{
-	_putchar(95);
+	_putchar('_');
 	}
-	-putchar('\n');
+        _putchar('\n');
 	}
 }
 	
