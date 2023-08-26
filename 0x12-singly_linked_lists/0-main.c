6ccc858933c47f4a9b333d165508b3ae0c80fd5f
Samuel Abayomi <samuelabayomi230@gmail.com>
2023-08-26 13:46:03 -0700

    check

--- 0x12-singly_linked_lists/0-main.c
+++ 0x12-singly_linked_lists/0-main.c
@@ -1,66 +1,40 @@
+#include <stdlib.h>
+#include <string.h>
 #include <stdio.h>
 #include "lists.h"
 
 /**
- * create_node - Creates a new node with the given data.
- * @str: The string to be stored in the node.
+ * main - check the code
  *
- * Return: A pointer to the new node, or NULL on failure.
+ * Return: Always 0.
  */
-list_t *create_node(char *str)
-{
-	list_t *new_node = malloc(sizeof(list_t));
-	if (new_node == NULL)
-		return (NULL);
-
-	new_node->str = str;
-	if (str != NULL)
-	{
-		unsigned int len = 0;
-		while (str[len])
-			len++;
-		new_node->len = len;
-	}
-	else
-	{
-		new_node->len = 0;
-	}
-
-	new_node->next = NULL;
-	return (new_node);
-}
-
-/**
- * free_list - Frees a list_t list.
- * @head: Pointer to the head of the linked list.
- */
-void free_list(list_t *head)
-{
-	list_t *temp;
-	while (head != NULL)
-	{
-		temp = head;
-		head = head->next;
-		free(temp);
-	}
-}
-
 int main(void)
 {
-	list_t *head = NULL;
-	size_t len;
+    list_t *head;
+    list_t *new;
+    list_t hello = {"World", 5, NULL};
+    size_t n;
 
-	head = create_node("Hello");
-	head->next = create_node("World");
-	head->next->next = create_node("of");
-	head->next->next->next = create_node("Linked");
-	head->next->next->next->next = create_node("Lists");
+    head = &hello;
+    new = malloc(sizeof(list_t));
+    if (new == NULL)
+    {
+        printf("Error\n");
+        return (1);
+    }
+    new->str = strdup("Hello");
+    new->len = 5;
+    new->next = head;
+    head = new;
+    n = print_list(head);
+    printf("-> %lu elements\n", n);
 
-	len = print_list(head);
-	printf("Number of nodes: %zu\n", len);
+    printf("\n");
+    free(new->str);
+    new->str = NULL;
+    n = print_list(head);
+    printf("-> %lu elements\n", n);
 
-	free_list(head);
-
-	return (0);
+    free(new);
+    return (0);
 }
-
